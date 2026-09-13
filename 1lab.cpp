ea381e396cbc8270a32b1c2cf5e27805d1db137d
Aleksandra718 <a71m8r@gmail.com>
2026-09-13 18:15:18 +0500

    меняем местами два элемента массива по их индексам

--- 1lab.cpp
+++ 1lab.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void fillArray(int (&arr)[10]) {      // arr ssilka na massiv is 10 el.
     cout << "Введите 10 элементов массива:" << endl;
 
-    for (int& : arr) {
+    for (int& x : arr) {
         cin >> x;         // prohodim po vsem el. massiva
     }
 }
@@ -16,3 +16,12 @@ void printArray(const int (&arr)[10]) {
     cout << endl;
 }
 
+void swapElements(int (&arr)[10], int index1, int index2) {
+    int temp = arr[index1];    // vrem. perem. 
+    arr[index1] = arr[index2]; // pervay zamena
+    arr[index2] = temp;        // vtoray zamena
+}
+
+int main() { // chtoby zapustilos
+    // ...
+}
