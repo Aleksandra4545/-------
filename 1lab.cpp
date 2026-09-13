930f59df89a2c01b02d61b1c6346b54a15526919
Aleksandra718 <a71m8r@gmail.com>
2026-09-13 17:53:05 +0500

    заполнение массива случайными числами

--- 1lab.cpp
+++ 1lab.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
 
-void fillArray(int (&arr)[10])
-{
-    for (auto &x : arr)
-    {
-        x = rand() % 100;
+void fillArray(int (&arr)[10]) {      // arr ssilka na massiv is 10 el.
+    cout << "Введите 10 элементов массива:" << endl;
+
+    for (int& : arr) {
+        cin >> x;         // prohodim po vsem el. massiva
     }
 }
 
